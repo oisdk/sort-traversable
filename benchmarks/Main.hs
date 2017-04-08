14e9cfe30946375654b81e264d48d99ce34b208a
Donnacha Oisín Kidney <mail@doisinkidney.com>
2017-04-08 15:37:27 +0100

    Changed up benchmarks, added stack.yaml

--- benchmarks/Main.hs
+++ benchmarks/Main.hs
@@ -1,70 +1,34 @@
-module Main where
-import Data.Traversable.Sort.PairingHeap (sortTraversable)
-import Criterion.Main
-import Data.List (sort)
-import qualified Data.Sequence as Seq
-import System.Random
-import Data.Foldable
+import           Criterion.Main
+import           System.Random
 
-main = do
-  g <- getStdGen
-  let million = take 1000000 $ randoms g :: [Int]
-      million' = Seq.fromList million
-      hundredthousand = take 100000 million
-      hundredthousand' = Seq.fromList hundredthousand
-      tenthousand = take 10000 million
-      tenthousand' = Seq.fromList tenthousand
-      thousand = take 1000 million
-      thousand' = Seq.fromList thousand
-  print $ sum $ map length
-            [million, hundredthousand,tenthousand,thousand]
-  print $ sum $ map (length . toList)
-            [million', thousand',hundredthousand',tenthousand',thousand']
-  print $ length $ toList hundredthousand
-  defaultMain [
-    bgroup "1000"
-      [ bgroup "list"
-        [ bench "Data.List"  $ nf sort thousand
-        , bench "HSTrav"     $ nf sortTraversable thousand
-        ]
-      , bgroup "sequence"
-        [ bench "sort"           $ nf Seq.sort thousand'
-        , bench "unstableSort"   $ nf Seq.unstableSort thousand'
-        , bench "HSTrav"         $ nf sortTraversable thousand'
-        ]
-      ]
-    , bgroup "10000"
-        [ bgroup "list"
-          [ bench "Data.List"  $ nf sort tenthousand
-          , bench "HSTrav"     $ nf sortTraversable tenthousand
-          ]
-        , bgroup "sequence"
-          [ bench "sort"           $ nf Seq.sort tenthousand'
-          , bench "unstableSort"   $ nf Seq.unstableSort tenthousand'
-          , bench "HSTrav"         $ nf sortTraversable tenthousand'
-          ]
-        ]
-    , bgroup "100000"
-        [ bgroup "list"
-          [ bench "Data.List"  $ nf sort hundredthousand
-          , bench "HSTrav"     $ nf sortTraversable hundredthousand
-          ]
-        , bgroup "sequence"
-          [ bench "sort"           $ nf Seq.sort hundredthousand'
-          , bench "unstableSort"   $ nf Seq.unstableSort hundredthousand'
-          , bench "HSTrav"         $ nf sortTraversable hundredthousand'
-          ]
-        ]
-    , bgroup "1000000"
-        [ bgroup "list"
-          [ bench "Data.List"  $ nf sort million
-          , bench "HSTrav"     $ nf sortTraversable million
-          ]
-        , bgroup "sequence"
-          [ bench "sort"           $ nf Seq.sort million'
-          , bench "unstableSort"   $ nf Seq.unstableSort million'
-          , bench "HSTrav"         $ nf sortTraversable million'
-          ]
-        ]
-      ]
+import           Data.Traversable.Sort.PairingHeap (sortTraversable)
+import           Data.List         (sort)
+
+import qualified Data.Sequence     as Seq
+
+import           Control.Monad     (replicateM)
+
+randInt :: IO Int
+randInt = randomIO
+
+testAtSize :: Int -> Benchmark
+testAtSize n =
+    bgroup
+        (show n)
+        [ env (replicateM n randInt) $
+          \lst ->
+               bgroup
+                   "list"
+                   [ bench "Data.List" $ nf sort lst
+                   , bench "HSTrav" $ nf sortTraversable lst]
+        , env (Seq.replicateM n randInt) $
+          \sq ->
+               bgroup
+                   "sequence"
+                   [ bench "sort" $ nf Seq.unstableSort sq
+                   , bench "unstableSort" $ nf Seq.sort sq
+                   , bench "HSTrav" $ nf sortTraversable sq]]
+
+main :: IO ()
+main = defaultMain $ map testAtSize [5000, 10000, 20000, 1000000]
 
