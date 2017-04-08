7deb6738b53c8befbdd66c05c62f897da1a8b6b2
Donnacha Oisín Kidney <mail@doisinkidney.com>
2017-04-08 15:45:46 +0100

    Switched out manual proof for TypeLits + ghc plugin

--- src/Data/Traversable/Sort/PairingHeap.hs
+++ src/Data/Traversable/Sort/PairingHeap.hs
@@ -1,34 +1,34 @@
 {-# LANGUAGE ScopedTypeVariables, GADTs, TypeOperators,
       RankNTypes, InstanceSigs, DataKinds #-}
+
+{-# OPTIONS_GHC -fplugin GHC.TypeLits.Normalise #-}
+
 module Data.Traversable.Sort.PairingHeap where
 import Data.Traversable.Sort.PairingHeap.IndexedPairingHeap (
     Heap
-  , Sized (..)
   , empty
   , singleton
   , merge
   , minView
   )
-import Data.Proxy
-import Data.Type.Equality ((:~:) (..))
-import Data.Traversable.Sort.PairingHeap.BasicNat
-        (type (+), Nat (..), plusAssoc, plusZero)
+
+import GHC.TypeLits
 
 -- | A heap of some size whose element have type @a@ and a
 -- function that, applied to any heap at least that large,
 -- will produce a result and the rest of the heap.
 data Sort a r where
-  Sort :: (forall n. Proxy n -> Heap (m + n) a -> (Heap n a, r))
+  Sort :: (forall n. Heap (m + n) a -> (Heap n a, r))
        -> !(Heap m a)
        -> Sort a r
 
 instance Functor (Sort x) where
   fmap f (Sort g h) =
-    Sort (\p h' -> case g p h' of (remn, r) -> (remn, f r)) h
+    Sort (\h' -> case g h' of (remn, r) -> (remn, f r)) h
   {-# INLINE fmap #-}
 
 instance Ord x => Applicative (Sort x) where
-  pure x = Sort (\_ h -> (h, x)) empty
+  pure x = Sort (\h -> (h, x)) empty
   {-# INLINE pure #-}
 
   -- Combine two 'Sort's by merging their heaps and composing
@@ -37,24 +37,22 @@ instance Ord x => Applicative (Sort x) where
   Sort f (xs :: Heap m x) <*> Sort g (ys :: Heap n x) =
     Sort h (merge xs ys)
     where
-      h :: forall o . Proxy o -> Heap ((m + n) + o) x -> (Heap o x, b)
-      h p v = case plusAssoc (size xs) (size ys) p of
-        Refl -> case f (Proxy :: Proxy (n + o)) v of { (v', a) ->
-                case g (Proxy :: Proxy o) v' of { (v'', b) ->
+      h :: forall o . Heap ((m + n) + o) x -> (Heap o x, b)
+      h v = case f v of { (v', a) ->
+                case g v' of { (v'', b) ->
                   (v'', a b)}}
   {-# INLINABLE (<*>) #-}
 
 -- Produce a 'Sort' with a singleton heap and a function that will
 -- produce the smallest element of a heap.
 liftSort :: Ord x => x -> Sort x x
-liftSort a = Sort (\_ h -> case minView h of (x, h') -> (h', x)) (singleton a)
+liftSort a = Sort (\h -> case minView h of (x, h') -> (h', x)) (singleton a)
 {-# INLINABLE liftSort #-}
 
 -- Apply the function in a 'Sort' to the heap within, producing a
 -- result.
 runSort :: forall x a . Sort x a -> a
-runSort (Sort f xs) = case plusZero (size xs) of
-    Refl -> snd $ f (Proxy :: Proxy 'Z) xs
+runSort (Sort (f :: Heap (m + 0) x -> (Heap 0 x, a)) xs) = snd $ f xs
 
 -- | Sort an arbitrary 'Traversable' container using a heap.
 sortTraversable :: (Ord a, Traversable t) => t a -> t a
--- src/Data/Traversable/Sort/PairingHeap/IndexedPairingHeap.hs
+++ src/Data/Traversable/Sort/PairingHeap/IndexedPairingHeap.hs
@@ -1,24 +1,23 @@
 {-# LANGUAGE DataKinds, ScopedTypeVariables, TypeOperators, GADTs, RoleAnnotations #-}
 {-# LANGUAGE BangPatterns #-}
+
+{-# OPTIONS_GHC -fplugin GHC.TypeLits.Normalise #-}
 module Data.Traversable.Sort.PairingHeap.IndexedPairingHeap (
     Heap
-  , Sized (..)
   , empty
   , singleton
   , insert
   , merge
   , minView
   ) where
-import Data.Traversable.Sort.PairingHeap.BasicNat
-import Data.Type.Equality ((:~:)(..))
-import Data.Coerce
-import Data.Type.Coercion
+
+import GHC.TypeLits
 
 -- | Okasaki's simple representation of a pairing heap, but with
 -- a size index.
 data Heap n a where
-  E :: Heap 'Z a
-  T :: a -> HVec n a -> Heap ('S n) a
+  E :: Heap 0 a
+  T :: a -> HVec n a -> Heap (1 + n) a
 
 -- Coercing a heap could destroy the heap property, so we declare both
 -- type parameters nominal.
@@ -26,53 +25,40 @@ type role Heap nominal nominal
 
 -- | A vector of heaps whose sizes sum to the index.
 data HVec n a where
-  HNil :: HVec 'Z a
+  HNil :: HVec 0 a
   HCons :: Heap m a -> HVec n a -> HVec (m + n) a
 
-class Sized h where
-  -- | Calculate the size of a structure
-  size :: h n a -> Natty n
-
-instance Sized Heap where
-  size E = Zy
-  size (T _ xs) = Sy (size xs)
-
-instance Sized HVec where
-  size HNil = Zy
-  size (HCons h hs) = size h `plus` size hs
-
 -- Produce an empty heap
-empty :: Heap 'Z a
+empty :: Heap 0 a
 empty = E
 
 -- Produce a heap with one element
-singleton :: a -> Heap ('S 'Z) a
+singleton :: a -> Heap 1 a
 singleton a = T a HNil
 
 -- Insert an element into a heap
-insert :: Ord a => a -> Heap n a -> Heap ('S n) a
+insert :: Ord a => a -> Heap n a -> Heap (1 + n) a
 insert x xs = merge (singleton x) xs
 {-# INLINABLE insert #-}
 
 -- Merge two heaps
 merge :: Ord a => Heap m a -> Heap n a -> Heap (m + n) a
 merge E ys = ys
-merge xs E = case plusZero (size xs) of Refl -> xs
+merge xs E = xs
 merge h1@(T x xs) h2@(T y ys)
-  | x <= y = case plusCommutative (size h2) (size xs) of Refl -> T x (HCons h2 xs)
-  | otherwise = case plusSucc (size xs) (size ys) of Refl -> T y (HCons h1 ys)
+  | x <= y = T x (HCons h2 xs)
+  | otherwise = T y (HCons h1 ys)
 {-# INLINABLE merge #-}
 
 -- Get the smallest element of a non-empty heap, and the rest of
 -- the heap
-minView :: Ord a => Heap ('S n) a -> (a, Heap n a)
+minView :: Ord a => Heap (1 + n) a -> (a, Heap n a)
 minView (T x hs) = (x, mergePairs hs)
 {-# INLINABLE minView #-}
 
 mergePairs :: Ord a => HVec n a -> Heap n a
 mergePairs HNil = E
-mergePairs (HCons h HNil) = case plusZero (size h) of Refl -> h
+mergePairs (HCons h HNil) = h
 mergePairs (HCons h1 (HCons h2 hs)) =
-  case plusAssoc (size h1) (size h2) (size hs) of
-    Refl -> merge (merge h1 h2) (mergePairs hs)
+    merge (merge h1 h2) (mergePairs hs)
 {-# INLINABLE mergePairs #-}
